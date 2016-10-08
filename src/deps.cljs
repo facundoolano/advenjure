f7ff18a6f706604d4bccf04f5810099d6dae6c36
Facundo Olano <facundo.olano@gmail.com>
2016-10-08 17:39:05 -0300

    update deps for optimizations

--- src/deps.cljs
+++ src/deps.cljs
@@ -4,9 +4,11 @@
     :provides ["jquery"]}
    {:file "jquery.terminal/jquery.terminal-0.11.10.js"
     :file-min "jquery.terminal/jquery.terminal-0.11.10.min.js"
+    :requires ["jquery"]
     :provides ["jquery.terminal"]}
    {:file "jquery.terminal/jquery.mousewheel.js"
     :file-min "jquery.terminal/jquery.mousewheel.min.js"
+    :requires ["jquery"]
     :provides ["jquery.mousewheel"]}
    {:file "xregexp/xregexp-all.js"
     :file-min "xregexp/xregexp-all.min.js"
