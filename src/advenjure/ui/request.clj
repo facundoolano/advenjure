c25683822fb194d0a52894d31e54c13fbad2bc90
Facundo Olano <facundo.olano@gmail.com>
2016-11-22 21:14:43 -0300

    added request function to ui

---
+++ src/advenjure/ui/request.clj
@@ -0,0 +1,11 @@
+(ns advenjure.ui.request
+  (:require [clojure.data.json :as json]))
+
+; FIXME handle errors
+(defn request
+  ([url] (request url {}))
+  ([url params]
+   (let [json? (= :json (:response-format params))
+         text (try (slurp url) (catch Exception e (if json? "{}" "")))]
+    (if json? (json/read-str text) text))))
+
