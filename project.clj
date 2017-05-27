5d1f8025eb216be91283d3f46884cb74271f083b
Facundo Olano <facundo.olano@gmail.com>
2017-05-26 21:21:30 -0300

    bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject advenjure "0.9.1-SNAPSHOT"
+(defproject advenjure "1.0.0-SNAPSHOT"
   :description "A text adveture engine"
   :url "https://github.com/facundoolano/advenjure"
   :license {:name "Eclipse Public License"
